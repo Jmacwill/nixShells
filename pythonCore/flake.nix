285deac70eee7e608213c1b4d4f25dabe18f94ee
cmacwill1 <charles.macwilliams@gmail.com>
2025-11-02 18:41:57 -0500

    init pythoncore

---
+++ pythonCore/flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "All this does is provide python to other flakes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    packages.x86_64-linux.pythonDefault = pkgs.python313;
+  };
+}
