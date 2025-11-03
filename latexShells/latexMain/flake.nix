a7db25d029dca5149e7087251caf2f9b53820021
cmacwill1 <charles.macwilliams@gmail.com>
2025-11-02 20:16:22 -0500

    latex testing

---
+++ latexShells/latexMain/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Provides texlive environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    packages.${system}.default = pkgs.texlive.combined.scheme-full;
+  };
+}
