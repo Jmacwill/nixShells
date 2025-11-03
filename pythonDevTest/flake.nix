4d64f72cfa37080fb5e7a01eb657dd177b52f5a1
cmacwill1 <charles.macwilliams@gmail.com>
2025-11-02 19:23:19 -0500

    test

--- pythonDevTest/flake.nix
+++ pythonDevTest/flake.nix
@@ -1,15 +1,31 @@
 {
-  description = "A very basic flake";
+  description = "Project flake combining python core and package collections";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    pythonCore.url = "github:cmacwill1/nixShells?dir=pythonCore";
+    pythonPackages.url = "github:cmacwill1/nixShells?dir=pythonBasicPackages";
+    pythonPackages2.url = "github:cmacwill1/nixShells?dir=pythonBasicPackages2";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+      inputs.nixpkgs.follows = "pythonCore";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, pythonCore, pythonPackages, pythonPackages2 }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    python = pythonCore.packages.${system}.default;
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    # Combine both sets of package lists
+    combinedPythonPackages = pythonPackages.lib.pythonPackages
+                           ++ pythonPackages2.lib.pythonPackages;
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        (python.withPackages (_: combinedPythonPackages))
+      ];
+    };
   };
 }
