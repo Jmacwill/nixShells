7bc8f8e1629e40ca7092c25e83083c487c7e7fec
cmacwill1 <charles.macwilliams@gmail.com>
2025-11-02 21:40:29 -0500

    working flake

--- testShell/flake.nix
+++ testShell/flake.nix
@@ -5,11 +5,12 @@
     pythonCore.url = "github:cmacwill1/nixShells?dir=pythonShells/pythonCore";
     pythonBasicPackages.url = "github:cmacwill1/nixShells?dir=pythonShells/pythonBasicPackages";
     pythonNotebook.url = "github:cmacwill1/nixShells?dir=pythonShells/pythonNotebook";
+    pythonAbaqus.url = "github:cmacwill1/nixShells?dir=pythonShells/pythonAbaqus";
     latexMain.url = "github:cmacwill1/nixShells?dir=latexShells/latexMain";
     nixpkgs.follows = "pythonCore/nixpkgs";
   };
 
-  outputs = { self, pythonCore, pythonBasicPackages, pythonNotebook, latexMain, nixpkgs }:
+  outputs = { self, pythonCore, pythonBasicPackages, pythonNotebook, pythonAbaqus, latexMain, nixpkgs }:
   let
     system = "x86_64-linux";
 
@@ -18,7 +19,8 @@
     latex = latexMain.packages.${system}.default;
 
     allPythonPackages = pythonBasicPackages.lib.pythonPackages
-                        ++ pythonNotebook.lib.pythonPackages;
+                        ++ pythonNotebook.lib.pythonPackages
+                        ++ pythonAbaqus.lib.pythonPackages;
   in
   {
     devShells.${system}.default = pkgs.mkShell {
