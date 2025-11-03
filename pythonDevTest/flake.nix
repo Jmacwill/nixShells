de6dd75f86e01786446aca7341a7c087aef84a7d
cmacwill1 <charles.macwilliams@gmail.com>
2025-11-02 20:22:46 -0500

    working dev shell

--- pythonDevTest/flake.nix
+++ pythonDevTest/flake.nix
@@ -4,15 +4,17 @@
   inputs = {
     pythonCore.url = "github:cmacwill1/nixShells?dir=pythonShells/pythonCore";
     pythonPackages.url = "github:cmacwill1/nixShells?dir=pythonShells/pythonBasicPackages";
+    latexMain.url = "github:cmacwill1/nixShells?dir=latexShells/latexMain";
     nixpkgs.follows = "pythonCore/nixpkgs";
   };
 
-  outputs = { self, pythonCore, pythonPackages, nixpkgs }:
+  outputs = { self, pythonCore, pythonPackages, latexMain, nixpkgs }:
   let
     system = "x86_64-linux";
 
     pkgs = nixpkgs.legacyPackages.${system};
     python = pythonCore.packages.${system}.default;
+    latex = latexMain.packages.${system}.default;
 
     allPythonPackages = pythonPackages.lib.pythonPackages;
   in
@@ -20,6 +22,7 @@
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = [
         (python.withPackages (pythonPkgs: allPythonPackages))
+        latex
       ];
     };
   };
