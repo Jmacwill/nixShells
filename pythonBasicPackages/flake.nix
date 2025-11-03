f7874eb60d2438e08dc6bb26843cd45bde047d1e
cmacwill1 <charles.macwilliams@gmail.com>
2025-11-02 19:48:03 -0500

    basic packages

--- pythonBasicPackages/flake.nix
+++ pythonBasicPackages/flake.nix
@@ -1,21 +1,26 @@
 {
-  description = "A very basic flake";
+  description = "Provides pythonPackages for dev shells";
 
   inputs = {
     pythonCore.url = "github:cmacwill1/nixShells?dir=pythonCore";
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-      inputs.nixpkgs.follows = "pythonCore";
-    };
+    nixpkgs.follows = "pythonCore";
   };
 
   outputs = { self, pythonCore, nixpkgs}:
   let
-    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in
   {
     lib.pythonPackages = with pkgs.python313Packages; [
-      numpy
+      numpy # Array & matrices
+      pandas # Data structures & tools
+      openpyxl # Python excel stuff
+      sympy # Symbolic math
+      scipy # Integral, solving differential, equations, optimizations)
+      tabulate # tables
+      matplotlib # plot & graphs
+      seaborn # heat maps, time series, violin plot
     ];
   };
 }
