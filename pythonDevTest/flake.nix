04f98bf42453dcd049f34fdd8182369d543db2e0
cmacwill1 <charles.macwilliams@gmail.com>
2025-11-02 19:52:43 -0500

    lock

--- pythonDevTest/flake.nix
+++ pythonDevTest/flake.nix
@@ -4,27 +4,25 @@
   inputs = {
     pythonCore.url = "github:cmacwill1/nixShells?dir=pythonCore";
     pythonPackages.url = "github:cmacwill1/nixShells?dir=pythonBasicPackages";
-    pythonPackages2.url = "github:cmacwill1/nixShells?dir=pythonBasicPackages2";
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
       inputs.nixpkgs.follows = "pythonCore";
     };
   };
 
-  outputs = { self, nixpkgs, pythonCore, pythonPackages, pythonPackages2 }:
+  outputs = { self, inputs, nixpkgs, }:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    python = pythonCore.packages.${system}.default;
 
-    # Combine both sets of package lists
-    combinedPythonPackages = pythonPackages.lib.pythonPackages
-                           ++ pythonPackages2.lib.pythonPackages;
+    pkgs = nixpkgs.legacyPackages.${system};
+    python = inputs.pythonCore.packages.${system}.default;
+
+    allPythonPackages = inputs.pythonPackages.lib.pythonPackages;
   in
   {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = [
-        (python.withPackages (_: combinedPythonPackages))
+        (python.withPackages (_: allPythonPackages))
       ];
     };
   };
